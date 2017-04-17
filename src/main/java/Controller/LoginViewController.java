34f058c09661ff507b7d4bb290911e6ca4a9c953
lcieply <lukaszcieply94@gmail.com>
2017-04-17 22:49:49 +0200

    LoginViewController - dispose() przy udanym logowaniu + kawalek tymczasowy

--- src/main/java/Controller/LoginViewController.java
+++ src/main/java/Controller/LoginViewController.java
@@ -17,7 +17,14 @@ public class LoginViewController {
         actionListener = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if(User.checkLogin(loginView.getLoginField().getText(), String.valueOf(loginView.getPasswordField().getPassword()))){
-                    System.out.print("Success!");
+                    loginView.getFrame().dispose();
+                    User user = User.getUserByLogin(loginView.getLoginField().getText());
+
+                    //Tutaj nowy controller do glownego okna - User potrzebny zeby go przeslac do modelu kolejnego z ktorego glowne okno bedzie korzystac (uprawnienia)
+                    JFrame frame = new JFrame();
+                    frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+                    frame.setVisible(true);
+                    //===================
                 }else{
                     JOptionPane.showMessageDialog(loginView.getFrame(),
                             "Wrong username or password!",
