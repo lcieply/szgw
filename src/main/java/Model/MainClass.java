3401a4961cc4a613518f4d702b15e121dddd0b6d
Marta G <tinywhiteboard@gmail.com>
2017-04-25 16:43:04 +0200

    Tabela leków

--- src/main/java/Model/MainClass.java
+++ src/main/java/Model/MainClass.java
@@ -1,21 +1,25 @@
 package Model;
 
 import Controller.LoginViewController;
+import Model.domain.Drug;
 import Model.domain.User;
 import View.LoginView;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.*;
 import javax.swing.*;
 import java.util.List;
 import java.util.logging.Level;
+
 // <property name="hibernate.hbm2ddl.auto" value="create" />
 
 public class MainClass {
     public static void main(String[] args){
-        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+        /*java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
         LoginView view = new LoginView();
         LoginViewController controller = new LoginViewController(view);
-        controller.control();
+        controller.control();*/
     }
 }
